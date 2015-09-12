aba168624c48028988629f1ec024862537c89055
Simone Zecca <simone.zecca.ita@gmail.com>
2015-09-12 22:26:59 +0200

    Added rest endpoint to search customer (still stubs are returned)

---
+++ src/main/java/net/szecc/server/rest/customers/model/RestCustomer.java
@@ -0,0 +1,8 @@
+package net.szecc.server.rest.customers.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class RestCustomer extends net.szecc.client.registration.Customer {
+
+}
